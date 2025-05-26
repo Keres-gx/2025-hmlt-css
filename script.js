c1fdde1923070e508bd0229ba527cd89ac523352
Keres-gx <keres.gx@gmail.com>
2025-05-26 17:32:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Ola mundo
+console.log('Ola, mundo!')
+
+// Função responsável por carregar o HTML e mostrar no Container
+function carregarPagina(pagina){
+    fetch(pagina)
+    .then((response) => response.text())
+    .then((html) => {
+        document.getElementById('content').innerHTML = html
+    })
+    .catch((error) => console.error("Erro ao carregar pagina: ",error))
+}
